f8c07615b5c2d3d3eb2f675c000584d816a45320
jazvillagra <jazvillagra.jv@gmail.com>
2025-05-07 19:10:57 +0200

    Add Prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 //main module file - specifies imports, controllers and providers
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
